e84a82d63e84d7e12f65935005fb7c9525fc658d
e-eman <97443726+e-eman@users.noreply.github.com>
2023-06-07 12:26:48 +0300

    Update test.sh

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-pat_token=$1
-repo_value=$2
-TESTPAT=$3
+repo_value=$1
+TESTPAT=$2
 
 echo "The token is"
 echo $GITHUB_TOKEN
 echo "TESTPAT: $TESTPAT"
-echo "TOKEN: $pat_token"
+#echo "TOKEN: $pat_token"
 echo "REPO: $repo_value"
