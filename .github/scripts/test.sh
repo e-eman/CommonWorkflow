cad07c2673cc9d9f9a39d840d8be1a0b9bc80c09
e-eman <97443726+e-eman@users.noreply.github.com>
2023-06-07 13:27:35 +0300

    Update test.sh

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -9,8 +9,7 @@
 # #echo "TOKEN: $pat_token"
 # echo "REPO: $repo_value"
 
-# JSONPAYLOAD=$(jq -n --arg comment "$COMMENTFIELD" '{"body": $comment}')
-# PR_NUMBER=$2
+
 TOKEN=$1
 COMMENTFIELD=$2
 REPO=$3
@@ -19,11 +18,12 @@ echo "TOKEN: $TOKEN"
 echo "REPO: $REPO"
 echo "Comment: $COMMENTFIELD"
 echo "PR_NUMBER: $PR_NUMBER"
+JSONPAYLOAD=$(jq -n --arg comment "$COMMENTFIELD" '{"body": $comment}')
 
-# curl -L \
-# -X POST \
-# -H "Accept: application/vnd.github+json" \
-# -H "Authorization: Bearer $TOKEN" \
-# -H "X-GitHub-Api-Version: 2022-11-28" \
-# "https://api.github.com/repos/$REPO/issues/$PR_NUMBER/comments" \
-# -d "$JSONPAYLOAD"
+curl -L \
+-X POST \
+-H "Accept: application/vnd.github+json" \
+-H "Authorization: Bearer $TOKEN" \
+-H "X-GitHub-Api-Version: 2022-11-28" \
+"https://api.github.com/repos/$REPO/issues/$PR_NUMBER/comments" \
+-d "$JSONPAYLOAD"
