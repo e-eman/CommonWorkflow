68d02f7e05bec48a909e85f090ab50385a52912f
e-eman <97443726+e-eman@users.noreply.github.com>
2023-06-07 13:17:18 +0300

    Update test.sh

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -9,20 +9,20 @@
 # #echo "TOKEN: $pat_token"
 # echo "REPO: $repo_value"
 
-COMMENTFIELD=$1
-JSONPAYLOAD=$(jq -n --arg comment "$COMMENTFIELD" '{"body": $comment}')
-PR_NUMBER=$2
-TOKEN=$3
-REPO=$4
+# COMMENTFIELD=$1
+# JSONPAYLOAD=$(jq -n --arg comment "$COMMENTFIELD" '{"body": $comment}')
+# PR_NUMBER=$2
+TOKEN=$1
+# REPO=$4
 echo "TOKEN $TOKEN"
-echo "REPO $REPO"
-echo "PR_NUMBER $PR_NUMBER"
-echo "Comment $COMMENTFIELD"
+# echo "REPO $REPO"
+# echo "PR_NUMBER $PR_NUMBER"
+# echo "Comment $COMMENTFIELD"
 
-curl -L \
--X POST \
--H "Accept: application/vnd.github+json" \
--H "Authorization: Bearer $TOKEN" \
--H "X-GitHub-Api-Version: 2022-11-28" \
-"https://api.github.com/repos/$REPO/issues/$PR_NUMBER/comments" \
--d "$JSONPAYLOAD"
+# curl -L \
+# -X POST \
+# -H "Accept: application/vnd.github+json" \
+# -H "Authorization: Bearer $TOKEN" \
+# -H "X-GitHub-Api-Version: 2022-11-28" \
+# "https://api.github.com/repos/$REPO/issues/$PR_NUMBER/comments" \
+# -d "$JSONPAYLOAD"
