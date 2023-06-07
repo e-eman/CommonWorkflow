a62aef02838a7cc9f436c867c2e75513d634eb94
e-eman <97443726+e-eman@users.noreply.github.com>
2023-06-07 12:58:22 +0300

    Update test.sh

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-repo_value=$1
-TESTPAT=$2
+# repo_value=$1
+# TESTPAT=$2
 
-echo "The token is"
-echo $GITHUB_TOKEN
-echo "TESTPAT: $TESTPAT"
-#echo "TOKEN: $pat_token"
-echo "REPO: $repo_value"
+# echo "The token is"
+# echo $GITHUB_TOKEN
+# echo "TESTPAT: $TESTPAT"
+# #echo "TOKEN: $pat_token"
+# echo "REPO: $repo_value"
+
+COMMENTFIELD=$1
+JSONPAYLOAD=$(jq -n --arg comment "$COMMENTFIELD" '{"body": $comment}')
+PR_NUMBER=$2
+TOKEN="$3"
+REPO=$4
+
+curl -L \
+-X POST \
+-H "Accept: application/vnd.github+json" \
+-H "Authorization: Bearer $TOKEN" \
+-H "X-GitHub-Api-Version: 2022-11-28" \
+"https://api.github.com/repos/$REPO/issues/$PR_NUMBER/comments" \
+-d "$JSONPAYLOAD"
